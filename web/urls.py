4def027a373b0850afa4e7d4f82e4debfc360644
guoshijiang <guoshijiang2012@163.com>
2022-08-29 00:45:15 +0800

    fix

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from web.views import index
+from web.views import index, join_submit
 
 
 urlpatterns = [
     path(r"", index, name="index"),
+    path(r"join_submit", join_submit, name="join_submit"),
 ]
--- web/views.py
+++ web/views.py
@@ -8,4 +8,6 @@ def index(request):
 
 
 def join_submit(request):
+    show_window = request.GET.get("show_window", "no")
+    print("show_window===", show_window)
     return render(request, "web/index.html", locals())
