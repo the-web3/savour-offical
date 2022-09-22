84cc6d92379d5074b5743a2dc40d73bb4a2b6faa
guoshijiang <guoshijiang2012@163.com>
2022-09-22 14:43:16 +0800

    change some css for en

--- savouroffical/settings.py
+++ savouroffical/settings.py
@@ -74,13 +74,13 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+#     }
+# }
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
